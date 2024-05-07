0c629beba723db7e9dc800c6b19c48167e8bdbed
Tnkma <onwusilikegodswill@gmail.com>
2024-05-07 16:31:46 +0100

    annotated the syntax

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -7,7 +7,7 @@ from typing import List
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(max_delay: int, n: int) -> List[float]:
+async def wait_n(n: int, max_delay: int) -> List[float]:
     """ waits for random delay time and returns it in seconds"""
     time_waited = await asyncio.gather(
             *tuple(map(lambda _: wait_random(max_delay), range(n))))
---
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+import time
+import asyncio
+
+wait_n = __import__('1-concurrent_coroutines').wait_n
+
+def measure_time(n: int, max_delay: int) -> float:
+    """ measures the runtime of wait_n"""
+    start_time = time.perf_counter()
+    asyncio.run(wait_n(n, max_delay))
+    end_time = time.perf_counter()
+    total_time = end_time - start_time
+    return total_time / n
