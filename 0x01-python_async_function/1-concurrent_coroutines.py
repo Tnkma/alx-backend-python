d038fc66571f40d2d99a408d2a24e0461dfb7c1c
Tnkma <onwusilikegodswill@gmail.com>
2024-05-07 15:36:10 +0100

    annotated the syntax

---
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+""" return a list of wait_times"""
+import asyncio
+from typing import List
+
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(max_delay: int, n: int) -> List[float]:
+    """ waits for random delay time and returns it in seconds"""
+    time_waited = await asyncio.gather(
+            *tuple(map(lambda _: wait_random(max_delay), range(n))))
+    return sorted(time_waited)
